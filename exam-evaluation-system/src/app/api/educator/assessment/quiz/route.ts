d03b056b252ccb4f2b1574eea45321f6b27fee0e
eg204115 <nethmipoornima2020@gmail.com>
2025-08-04 07:03:29 +0530

    Implement API to create a quiz

---
+++ exam-evaluation-system/src/app/api/educator/assessment/quiz/route.ts
@@ -0,0 +1,91 @@
+import { prisma } from '@/lib/prisma';
+import { NextRequest, NextResponse } from 'next/server';
+import { v4 as uuidv4 } from 'uuid';
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const {
+      assessmentId,
+      type = 'quiz',
+      title,
+      description,
+      instructions,
+      duration,
+      deadline,
+      moduleId,
+      createdBy,
+      questions,
+    } = body;
+    // moduleId || !createdBy ||
+
+    if (!assessmentId || !title ||  !Array.isArray(questions)) {
+      return NextResponse.json(
+        { success: false, message: 'Missing required fields' },
+        { status: 400 }
+      );
+    }
+
+    const now = new Date();
+
+    // Upsert the assessment
+    const assessment = await prisma.assessment.upsert({
+      where: { assessment_id: assessmentId },
+      update: {
+        title,
+        description,
+        instructions,
+        duration,
+      },
+      create: {
+        assessment_id: assessmentId,
+        type,
+        title,
+        description,
+        instructions,
+        duration,
+        deadline: deadline || new Date(),
+        module_id: moduleId,
+        created_by: createdBy,
+      },
+    });
+
+    // Create new question paper
+    const questionPaperId = uuidv4();
+    const questionPaper = await prisma.question_Paper.create({
+      data: {
+        question_paper_id: questionPaperId,
+        assessment_id: assessmentId,
+        file_url: '',
+        created_on: now,
+      },
+    });
+
+    // Bulk create new questions
+    const formattedQuestions = questions.map((q: any, index: number) => ({
+      assessment_id: assessmentId,
+      question_paper_id: questionPaperId,
+      type: q.questionType === 'short_answer' ? 'SHORT' : 'MCQ',
+      question_number: `${index + 1}`,
+      question: q.questionText,
+      model_answer: q.expectedAnswer || '',
+      mcq_answer_options: q.options || [],
+      marks_allowed: Math.round(q.marks || 0),
+    }));
+
+    await prisma.question.createMany({ data: formattedQuestions });
+
+    return NextResponse.json({
+      success: true,
+      message: 'Assessment and questions saved',
+      assessment,
+      questionPaper
+    });
+  } catch (err) {
+    console.error('[POST /api/educator/assessment/questions]', err);
+    return NextResponse.json(
+      { success: false, message: 'Internal Server Error' },
+      { status: 500 }
+    );
+  }
+}
